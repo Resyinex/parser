38ddfe49d31d221c58eba5d785649488be3de73f
Texyser <45159492+Texyser@users.noreply.github.com>
2020-10-14 21:54:36 +0600

    Added ru comments

--- code.py
+++ code.py
@@ -1,3 +1,9 @@
+"""
+Программа для скрапинга информации с сайта https://www.zoopla.co.uk/
+И сохранения данных в файле EXCEL
+"""
+
+
 from openpyxl import Workbook
 import time
 import requests
@@ -29,6 +35,7 @@ session.headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) Appl
 
 
 def load_page(url, parameters):
+    """Обращается по указанному URL и возвращает его, если возникает ошибка, то ждёт 5 сек и пытается снова"""
     while True:
         try:
             request = session.get(url=url, params=parameters)
@@ -39,6 +46,8 @@ def load_page(url, parameters):
 
 
 def load_page_text(url, parameters):
+    """Обращается по указанному URL и возвращает только его текстовую часть,
+    если возникает ошибка, то ждёт 5 сек и пытается снова"""
     while True:
         try:
             request = session.get(url=url, params=parameters)
@@ -49,6 +58,8 @@ def load_page_text(url, parameters):
 
 
 def get_detail_url(list_of_url_endings):
+    """Принимает список из окончаний сайта, соединияет его с главным URL
+    и возвращает список URL-ов для каждого отдельного объявления"""
     main_url = 'https://www.zoopla.co.uk/'
     list_for_full_urls = []
     for each_ending in list_of_url_endings:
@@ -62,21 +73,27 @@ def get_detail_url(list_of_url_endings):
 # ---------------------------------------------------------------------------------------------------------------------
 
 def soup_page(requested_page, parsing_limits=None):
+    """Переводит в XML полученный с помощью requests текст страницы и возвращает его"""
     xml_text = BeautifulSoup(requested_page, 'lxml', parse_only=SoupStrainer(name=parsing_limits[0], attrs=parsing_limits[1]))
     return xml_text
 
 
 def find_tag(xml_text, search_options):
+    """В XML тексте ищет указанные тег с аттрибутом и возвращает его в виде строки"""
     tag = xml_text.find(name=search_options[0], attrs=search_options[1])
     return tag
 
 
 def find_tags(xml_text, search_options):
+    """В XML тексте ищет группу схожих тегов по указанныем свойствам и возвращает их в виде списка"""
     tags = xml_text.find_all(name=search_options[0], attrs=search_options[1])
     return tags
 
 
 def get_value(tag, search_value_in):
+    """Принимает весь тег и аттрибут/тег значение которого нужно получить
+    проверяет является ли тег пустым; из тега или из аттрибута извлекается значение
+    и возвращается искомое значения"""
     if tag is None:
         value = None
     elif tag.name == search_value_in:
@@ -87,6 +104,9 @@ def get_value(tag, search_value_in):
 
 
 def page_parsing(url, url_parameters, parsing_limits, search_options, value_options):
+    """Запрашивает страницу сайта, переводит её,
+    ищет ссылку для перехода на детальную страницу каждого объявления
+    собирает все ссылки в список и возвращает их"""
     requesting_text = load_page_text(url, url_parameters)
     souping = soup_page(requesting_text, parsing_limits)
     found_tags = find_tags(souping, search_options)
@@ -98,14 +118,16 @@ def page_parsing(url, url_parameters, parsing_limits, search_options, value_opti
 
 
 def detail_parsing(url, url_parameters, parsing_limits, search_options, value_options):
+    """Запрашивает страницу сайта, переводит её,
+    ищет всю необходимую информацию об объявлении и возвращает её в виде списка"""
     requesting_page = load_page(url, url_parameters)
     souping = soup_page(requesting_page.text, parsing_limits)
     requesting_date = requesting_page.headers['Date']
-    extended_info = [url, requesting_date]
+    extended_info = [url, requesting_date]  # дополнительная информация
     values_result = []
-    for each_option in search_options:
-        found_tag = find_tag(souping, each_option)
-        found_value = get_value(found_tag, value_options[search_options.index(each_option)])
+    for each_option in search_options:  # ищется несколько тегов по очереди
+        found_tag = find_tag(souping, each_option)  # поиск тег
+        found_value = get_value(found_tag, value_options[search_options.index(each_option)])  # очерёдность опций для поиска тегов и значений совпадает
         values_result.append(found_value)
     detail_parsing_result = values_result + extended_info
     return detail_parsing_result
@@ -116,6 +138,7 @@ def detail_parsing(url, url_parameters, parsing_limits, search_options, value_op
 # ---------------------------------------------------------------------------------------------------------------------
 
 def price_corrector(raw_price, raw_price_pft, raw_price_pcm):
+    """Корректирует значения цен, убирая все ненужные значения"""
     if raw_price.isalpha() or raw_price is None:
         corrected_price = raw_price
     else:
@@ -132,6 +155,7 @@ def price_corrector(raw_price, raw_price_pft, raw_price_pcm):
 
 
 def address_converter(address):
+    """Разбивает строку с указанным адресом"""
     con_address, con_city, con_postal_code = None, None, None
     if address is not None:
         street = str()
@@ -154,6 +178,7 @@ def address_converter(address):
 
 
 def get_identifier(url):
+    """Извлекает идентификатор из URL"""
     first_split = url.split("?")
     second_split = first_split[0].split("/")
     identifier = second_split[-1]
@@ -161,6 +186,7 @@ def get_identifier(url):
 
 
 def description_corrector(desc):
+    """Корректирует описанин"""
     if desc is None:
         return None
     replacing_desc = desc.replace("\n", "")
@@ -169,6 +195,7 @@ def description_corrector(desc):
 
 
 def surface_corrector(surf):
+    """Корректирует площадь"""
     if surf is None:
         return None
     rep_surf = surf.replace("From", "")
@@ -177,6 +204,7 @@ def surface_corrector(surf):
 
 
 def value_sort(detail_values):
+    """Сортирует все принятые данные и распологает в необходимом порядке"""
     price, price_pft, price_pcm = price_corrector(detail_values[0], detail_values[1], detail_values[2])
     address, city, postal_code = address_converter(detail_values[3])
     description = description_corrector(detail_values[5])
@@ -202,6 +230,16 @@ PROPOSE_URLS = [{'page_size': 25, 'pn': 1},
                 ['a', 'listing-results-price text-price'],
                 'href']
 
+"""
+price_tag = ['p', 'ui-pricing__main-price ui-text-t4']
+price_pft_tag = ['p', 'ui-pricing__area-price']
+price_pcm_tag = ['p', 'ui-pricing__alt-price']
+address_tag = ['h2', 'ui-property-summary__address']
+
+m2_tag = ['span', 'dp-features-list__text']
+description_tag = ['div', 'dp-description__text']
+agent_tag = ['h4', 'ui-agent__name']
+"""
 PROPOSE_DETAILS = [None,
                    ['div', {'class': 'ui-layout'}],
                    [['p', 'ui-pricing__main-price ui-text-t4'],
@@ -241,6 +279,7 @@ def run_program(page_amount, propose_urls, propose_details, regions):
                                            parsing_limits=page_amount[1],
                                            search_options=page_amount[2],
                                            value_options=page_amount[3])
+            # указание последней страницы и проверка на случай, если траниц больше нет
             if len(amount_of_pages) == 0:
                 last_page = 1
             else:
